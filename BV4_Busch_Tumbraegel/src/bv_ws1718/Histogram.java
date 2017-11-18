a0323d0dae3ca5595d12a922acfa182b882425a6
J. Busch <josobu33@gmail.com>
2017-11-18 15:33:41 +0100

    histogramm is working

--- BV4_Busch_Tumbraegel/src/bv_ws1718/Histogram.java
+++ BV4_Busch_Tumbraegel/src/bv_ws1718/Histogram.java
@@ -39,7 +39,7 @@ public class Histogram {
 		gc.clearRect(0, 0, grayLevels, maxHeight);
 		gc.setLineWidth(1);
 
-		//draw histogram[] into the gc graphic context
+		// draw histogram[] into the gc graphic context
 		
 		// Remark: This is some dummy code to give you an idea for graphics drawing		
 		double shift = 0.5;
