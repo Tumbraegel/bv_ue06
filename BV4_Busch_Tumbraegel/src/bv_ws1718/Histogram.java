3fa76e996d9244c6e1c48450e69d0cb9ccb06a1b
Tumbrägel <s0558529@htw-berlin.de>
2017-11-25 15:30:44 +0100

    histogram changes

--- BV4_Busch_Tumbraegel/src/bv_ws1718/Histogram.java
+++ BV4_Busch_Tumbraegel/src/bv_ws1718/Histogram.java
@@ -27,8 +27,7 @@ public class Histogram {
 	}
 	
 	public void update(RasterImage image, Rectangle selectionRect, ObservableList<StatsProperty> statsData) {
-		//calculate histogram[] out of the gray values found in the images selectionRect
-		
+		//calculate histogram[] out of the gray values found in the images selectionRect		
 		int rectY = (int)selectionRect.getY();
 		int rectX = (int)selectionRect.getX();
 		int rectWidth = (int)selectionRect.getWidth();
@@ -36,32 +35,42 @@ public class Histogram {
 		
 		for(int picY = rectY; picY < rectY+rectHeight; picY++) {
 			for(int picX = rectX; picX < rectX+rectWidth; picX++) {
-				int grayLevel = (image.argb[picY*rectWidth+rectX] >> 16) & 0xff;
+				int grayLevel = (image.argb[picY*rectWidth+picX] >>16) & 0xff;
 				histogram[grayLevel]++;
 			}
 		}
-		
-		// Remark: Please ignore statsData in Exercise 4. It will be used in Exercise 5.
-		
+		// Remark: Please ignore statsData in Exercise 4. It will be used in Exercise 5.	
 	}
 	    
 	public void draw() {
+		//TO DO: draw histogram[] into the gc graphic context
 		gc.clearRect(0, 0, grayLevels, maxHeight);
-		gc.setLineWidth(1);
-
-		// draw histogram[] into the gc graphic context
+		gc.setStroke(Color.DARKRED);
+		gc.setLineWidth(1);	
 		
+		//get max value of histogram
+		int max = histogram[0];
+		for(int i=0;i<histogram.length;i++){
+			int current = histogram[i];
+			if(current>max){
+				max = current;
+			}
+		}
 		
-		// Remark: This is some dummy code to give you an idea for graphics drawing		
 		double shift = 0.5;
+		double ratio = (double)maxHeight/max;
+		
+		for(int index=1; index<grayLevels; index++){
+			double adaptedValue = (double)(histogram[index]+shift)*ratio;
+			gc.strokeLine(index+shift,maxHeight+shift,index+shift,maxHeight-adaptedValue+shift);
+		}
+		// Remark: This is some dummy code to give you an idea for graphics drawing		
 		// note that we need to add 0.5 to all coordinates to get a one pixel thin line 
-		gc.setStroke(Color.GREEN);
-		gc.strokeLine(128 + shift, shift, 128 + shift, maxHeight + shift);
-		gc.strokeLine(shift, maxHeight/2 + shift, grayLevels + shift, maxHeight/2 + shift);
-		gc.setStroke(Color.ORANGE);
-		gc.strokeLine(shift, shift, grayLevels + shift, maxHeight + shift);
-		gc.strokeLine(grayLevels + shift, shift, shift, maxHeight + shift);
-
+//		gc.setStroke(Color.GREEN);
+//		gc.strokeLine(128 + shift, shift, 128 + shift, maxHeight + shift);
+//		gc.strokeLine(shift, maxHeight/2 + shift, grayLevels + shift, maxHeight/2 + shift);
+//		gc.setStroke(Color.ORANGE);
+//		gc.strokeLine(shift, shift, grayLevels + shift, maxHeight + shift);
+//		gc.strokeLine(grayLevels + shift, shift, shift, maxHeight + shift);
 	}
-    
 }
